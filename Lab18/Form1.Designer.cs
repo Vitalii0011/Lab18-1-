51a7cfa3400da8c6bbff0fd85b99b8d376a61304
Виталий <@DESKTOP-OASAN1C>
2025-04-24 23:30:34 +0300

    Добавьте файлы проекта.

---
+++ Lab18/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Lab18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(206, 43);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(394, 22);
+            this.txtArray.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть масив:";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(49, 115);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(142, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Обробити масив";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(251, 115);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.Size = new System.Drawing.Size(238, 119);
+            this.txtConsole.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtConsole;
+    }
+}
+
---
+++ Lab18/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Lab18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            txtConsole.Clear();
+
+            // 1. Зчитування масиву
+            string[] parts = txtArray.Text.Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0)
+            {
+                txtConsole.Text = "Введіть дійсні числа через пробіл.";
+                return;
+            }
+
+            double[] array;
+            try
+            {
+                array = parts.Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+            }
+            catch
+            {
+                txtConsole.Text = "Невірний формат числа. Використовуйте крапку як роздільник дробової частини (напр. 3.14).";
+                return;
+            }
+
+            // 2. Номер максимального елемента
+            int maxIndex = Array.IndexOf(array, array.Max());
+            txtConsole.AppendText($"Номер (індекс) максимального елемента: {maxIndex}\r\n");
+
+            // 3. Добуток між першим і другим нулем
+            int firstZero = Array.IndexOf(array, 0);
+            int secondZero = Array.FindIndex(array, firstZero + 1, x => x == 0);
+
+            if (firstZero >= 0 && secondZero > firstZero + 1)
+            {
+                double product = 1;
+                for (int i = firstZero + 1; i < secondZero; i++)
+                    product *= array[i];
+
+                txtConsole.AppendText($"Добуток між першим і другим нулем: {product}\r\n");
+            }
+            else if (firstZero >= 0 && secondZero == firstZero + 1)
+            {
+                txtConsole.AppendText("Між нулями немає елементів. Добуток = 1\r\n");
+            }
+            else
+            {
+                txtConsole.AppendText("Не знайдено двох нулів для обчислення добутку.\r\n");
+            }
+
+            // 4. Перетворення масиву
+            var oddPos = array.Where((x, i) => i % 2 == 0).ToArray();  // індекси 0,2,4... (непарні позиції)
+            var evenPos = array.Where((x, i) => i % 2 != 0).ToArray(); // індекси 1,3,5... (парні позиції)
+
+            double[] transformed = oddPos.Concat(evenPos).ToArray();
+
+            txtConsole.AppendText("Перетворений масив: " + string.Join(" ", transformed.Select(x => x.ToString(CultureInfo.InvariantCulture))) + "\r\n");
+        }
+    }
+}
+
+    
+
